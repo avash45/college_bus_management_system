bfbfd4489f7cee9ebf1883410dd8d06c18bf908b
avash45 <avaneesh4564@gmail.com>
2022-12-10 11:57:09 +0530

    DEc 10

--- index.js
+++ index.js
@@ -1,57 +1,234 @@
-const express = require("express")    //variable
-const bodyParser = require("body-parser")   //variable
+//=========================================Import necessary modules and create objects=========
+
+const express = require("express")
+const bodyParser = require("body-parser")
 const path = require('path')
-const app = express()    //object created
+const mysql = require('mysql')
+const sessions = require('express-session')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, '/views')))
+app.use(bodyParser.urlencoded({ extended: true }))
 
-// app.set('view engine','ejs')
+//==============================================Session===========================================
 
-app.use(express.static(path.join(__dirname, '/public')))
-app.use(bodyParser.urlencoded({extended:true}))
+var session;
+app.use(sessions({
+    secret: "secretkey",
+    saveUninitialized: true,
+    resave: false
+}));
+
+//=========================================Database connection=======================================
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'lofilines2'
+});
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('SQL Connected')
+});
 
 
-// Homepage
-app.get("/",function(req, res){
-    res.sendFile(__dirname+"/public/html/homepage.html");
+//===============================================Homepage====================================================
+app.get("/", function (req, res) {
+    req.session.destroy;
+    var springedge = require('springedge');
+
+
+    springedge.messages.send(params, 5000, function (err, response) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log(response);
+    });
+    res.render("html/homepage.ejs");
 })
-app.get("/forgot-password",function(req, res){
-    res.sendFile(__dirname+"/public/html/forgot-password.html");
+
+app.get("/routes", function (req, res) {
+    res.render("html/routes.ejs");
+});
+
+app.get("/gallery", function (req, res) {
+    res.render("html/gallery.ejs");
 })
 
 
-// Admin
-app.get("/admin/login",function(req, res){
-    res.sendFile(__dirname+"/public/html/admin/login.html");
+//=====================================Passenger login=====================================
+
+
+app.get("/passenger/login", function (req, res) {
+    res.render("html/passenger/login.ejs")
+});
+
+app.post("/passenger/login", function (req, res) {
+    var email = req.body.email
+    var password = req.body.password
+    let sql = "SELECT * FROM USERS WHERE email='" + email + "' AND password='" + password + "';";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (result.length == 0) {
+            res.send("No accounts found");
+        }
+        else {
+            session = req.session;
+            session.userid = email;
+            res.render('html/passenger/home.ejs')
+        }
+    });
+});
+
+
+app.get("/passenger/signup", function (req, res) {
+    res.render("html/passenger/signup.ejs");
 })
-app.get("/admin/home",function(req, res){
-    res.sendFile(__dirname+"/public/html/admin/home.html");
+
+app.post("/passenger/signup", (req, res) => {
+    fname = req.body.fname;
+    phoneno = req.body.phoneno;
+    designation = req.body.inlineRadioOptions
+    email = req.body.email;
+    usn = req.body.usn;
+    addresss = req.body.addresss;
+    password = req.body.password;
+
+    console.log(email + "  " + password + "  " + fname + "  " + phoneno + "  " + designation + "  " + usn + "  " + addresss
+    );
+
+    let sql = ""
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (result.length == 0) {
+            console.log("No accounts found");
+            res.render("html/admin/login")
+        }
+        else {
+            res.render('html/admin/home.ejs')
+        }
+    });
+});
+
+app.get("/admin/login", function (req, res) {
+    res.render("html/admin/login.ejs");
+});
+
+app.get("/forgot-password", function (req, res) {
+    res.render("html/forgot-password.html");
 })
-app.get("/admin/routes",function(req, res){
-    res.sendFile(__dirname+"/public/html/admin/routes.html");
+
+//------------------------------------Admin---------------------------------------------------------------
+
+
+app.post("/admin/login", (req, res) => {
+    email = req.body.email
+    password = req.body.password
+
+    console.log(email + "   " + password);
+
+
+    let sql = "SELECT * FROM ADMIN WHERE admin_email='" + email + "' AND admin_password='" + password + "';";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (result.length == 0) {
+            console.log("No accounts found");
+            res.render("html/admin/login")
+        }
+        else {
+            res.render('html/admin/home.ejs')
+            req.session.login = "true"
+        }
+    });
 })
-app.get("/admin/new-registration",function(req, res){
-    res.sendFile(__dirname+"/public/html/admin/newreg.html");
+
+app.get("/admin/bus", function (req, res) {
+    if (flag = 2) {
+        res.render("html/admin/bus.ejs");
+    }
+    else {
+        res.send("ADmin nahi hai yu")
+    }
+})
+
+app.get("/admin/adminroutes", function (req, res) {
+    res.render("html/admin/adminroutes.ejs");
 })
 
 
+
+app.get("/admin/newreg", function (req, res) {
+    res.render("html/admin/newreg.ejs");
+})
+
+
+//------------------------------------------------------------------------------------------------------
 // Passenger
-app.get("/passenger/login",function(req, res){
-    res.sendFile(__dirname+"/public/html/passenger/login.html");
+
+
+app.post("/passenger/login", (req, res) => {
+    email = req.body.email
+    password = req.body.password
+
+    let sql = "SELECT * FROM PASSENGER WHERE passenger_email='" + email + "' AND passenger_password='" + password + "';";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        if (result.length == 0) {
+            console.log("No accounts found");
+            res.render("html/passenger/login")
+        }
+        else {
+            pname = result[0].Passenger_name
+            res.render('html/passenger/home.ejs', { data: { passenger_name: pname } })
+            req.session.login = "passenger"
+        }
+    });
 })
-app.get("/passenger/home",function(req, res){
-    res.sendFile(__dirname+"/public/html/passenger/home.html");
+
+app.get("/passenger/home", function (req, res) {
+    let sql = "SELECT * FROM PASSENGER WHERE passenger_email='" + email + "' AND passenger_password='" + password + "';";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        let passenger_name = result[0].Passenger_name;
+
+    })
+});
+
+
+app.get("/passenger/viewroute", function (req, res) {
+    res.render("html/passenger/viewroute.ejs");
+});
+
+app.get("/passenger/profile", function (req, res) {
+    let sql = "SELECT * FROM PASSENGER ;";
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        busn = result[0].bus_no
+        pasid = result[0].pass_id
+        addr = result[0].address
+        usnn = result[0].usn
+        passname = result[0].Passenger_name
+        phno = result[0].Phone_no
+        phmail = result[0].passenger_email
+        des = reult[0].designation
+        console.log("COnnected")
+        res.render("html/passenger/profile.ejs", { data: { passengername: passname, designations: des } })
+    });
+});
+
+
+app.listen("3000", function () {
+    console.log("Hey iM listening: 3000")
 })
 
+//=======================================================================================
 
-
-
-
-
-// app.post("/signin",function(req,res){
-//     var username = req.body.lname
-//     var password =req.body.password
-//     console.log(username+" "+password)
-// })
-
-app.listen("3000",function(){
-    console.log("Hey iM listening")
-})
+    // var params = {
+    //     'apikey': '', // API Key 
+    //     'sender': 'Av', // Sender Name 
+    //     'to': [
+    //         '9686104291'  //Moblie Number 
+    //     ],
+    //     'message': 'Hey'
+    // };
